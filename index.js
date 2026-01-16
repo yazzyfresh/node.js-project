9cc0db98994782c175ef1bded9c3fefc092369e7
yazzyfresh <yazzy06ramirez@aol.com>
2026-01-16 12:48:29 -0500

    some work that was compleated today

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// //blocking synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: \n${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
 
-const textOut = `This is what we know about the avocado: \n${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!'); 
+//non-blocking asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+  });
+});
+console.log("Will read file!");
