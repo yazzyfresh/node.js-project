905d40026995b10cba611f41188d1264cf75c8fa
yazzyfresh <yazzy06ramirez@aol.com>
2026-01-21 13:05:25 -0500

    attempting to find error

--- index.js
+++ index.js
@@ -34,21 +34,25 @@ const url = require("url");
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
-if (pathName === '/' || pathName === '/overview') {
-  res.end("This is the OVERVIEW");
-} else if (pathName === '/product') {
-  res.end('This is the PRODUCT');
-}
-else if (pathName === '/api') {
-  res.end('API');
-}
-else {
-  res.writeHead(404, {
-    'Content-type': 'text/html',
-    'my-own-header': 'hello-world'
-  });
-  res.end('<h1>Page not found!</h1>');
-}
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+
+    fs.readFile("${__dirname}/dev-data/data.json", "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      res.writeHead(200, { 'Content-type': 'application/json' });
+      res.end(data);
+    });
+    
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
