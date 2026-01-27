5898396ad597851bf78cbad15ffe37c056759a8d
yazzyfresh <yazzy06ramirez@aol.com>
2026-01-27 10:34:37 -0500

    work done prior to lunch

--- index.js
+++ index.js
@@ -32,10 +32,9 @@ const url = require("url");
 ////////////////////////////////////////////////////////////
 //Server
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObj = JSON.parse(data);
+  const dataObj = JSON.parse(data);
 
-
-const server = http.createServer((req, res) => {
+  const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === "/" || pathName === "/overview") {
@@ -43,12 +42,8 @@ const server = http.createServer((req, res) => {
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
   } else if (pathName === "/api") {
-
-    fs.readFile("${__dirname}/dev-data/data.json", "utf-8", (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, { 'Content-type': 'application/json' });
-      res.end(data);
-    });
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
 
   } else {
     res.writeHead(404, {
